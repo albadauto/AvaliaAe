b9faa89a028b4c62be0432a9c0554be9e77bc5bb
albadev <joseadauto923@gmail.com>
2022-10-23 11:07:18 -0300

    Search certification

---
+++ Controllers/SearchCertificationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AvaliaAe.Controllers
+{
+    public class SearchCertificationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
