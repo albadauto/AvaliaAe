964c4e685f968648c8d3abcc5ae01d5466dccf32
albadev <joseadauto923@gmail.com>
2022-10-23 10:07:19 -0300

    Instituitions

---
+++ Controllers/InstitutionsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AvaliaAe.Controllers
+{
+    public class InstitutionsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
