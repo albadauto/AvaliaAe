28c58d324a6332414300877ea5218ee7623f5490
albadev <joseadauto923@gmail.com>
2022-10-29 20:30:12 -0300

    Doubt | Home | Ranking | Layout | WeAre

---
+++ Controllers/RankingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AvaliaAe.Controllers
+{
+    public class RankingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
