b5c84b5f7c6431283034e5ed8cba91e9fe79aa3b
albadev <joseadauto923@gmail.com>
2022-11-02 14:35:44 -0300

    Forgot password

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,6 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+import bootstrap from 'bootstrap'
+const toastElList = document.querySelectorAll('.toast')
+const toastList = [...toastElList].map(toastEl => new bootstrap.Toast(toastEl, option))
