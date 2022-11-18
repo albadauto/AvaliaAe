ef1ebf0c70eeb1a02da71860623b1438e88358a1
albadev <joseadauto923@gmail.com>
2022-11-18 09:08:33 -0300

    Page to associate

---
+++ Controllers/ToAssociateController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AvaliaAe.Controllers
+{
+    public class ToAssociateController : Controller
+    {
+        public IActionResult Index(int id)
+        {
+            return View();
+        }
+
+    }
+}
