6bdf0b0a7f5a21f74def2df60de3bce7c76862ba
José Adauto <60900449+albadauto@users.noreply.github.com>
2023-01-08 23:19:16 -0300

    Procedures for tests

--- Tests/SQL/ProceduresCreate.sql
+++ Tests/SQL/ProceduresCreate.sql
@@ -14,4 +14,38 @@ BEGIN
 	END
 		
 END;
+--Procedure para deixar todas as associações desaprovadas
+IF EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND OBJECT_ID = OBJECT_ID('dbo.SETALLASSOCIATIONINPENDENT'))
+BEGIN 
+	DROP PROCEDURE dbo.SETALLASSOCIATIONINPENDENT
+END
+GO
+
+CREATE PROCEDURE SETALLASSOCIATIONINPENDENT
+AS 
+BEGIN
+	IF EXISTS(SELECT * FROM Associations)
+	BEGIN
+		UPDATE [Associations] SET STATUS = 'P'
+	END
+END;
+
+ --Procedure feita para setar status de um usuário na tabela association
+IF EXISTS(SELECT * FROM sys.objects WHERE type = 'P' AND OBJECT_ID = OBJECT_ID('dbo.SETASSOCIATIONOFUSER'))
+BEGIN
+	DROP PROCEDURE dbo.SETASSOCIATIONOFUSER
+END
+GO
+CREATE PROCEDURE SETASSOCIATIONOFUSER(@UserId int, @Status varchar(1))
+AS
+BEGIN
+	IF EXISTS(SELECT * FROM [User] WHERE Id = @UserId)
+	BEGIN
+		UPDATE [Associations] SET STATUS = @Status WHERE UserModelId = @UserId
+	END
+	SELECT * FROM [Associations] WHERE UserModelId = @UserId;
+END
+GO
+
+EXEC SETASSOCIATIONOFUSER 2006, 'P'
 
