3aed1499bd58265c8d91f0bee0cccc6f4b58dbe9
José Adauto <60900449+albadauto@users.noreply.github.com>
2023-01-22 11:48:00 -0300

    Calculation Helper

---
+++ Helpers/CalculationHelper.cs
@@ -0,0 +1,12 @@
+﻿using AvaliaAe.Helpers.Interfaces;
+
+namespace AvaliaAe.Helpers
+{
+    public class CalculationHelper : ICalculationHelper
+    {
+        public float CalculateAverage(object notes)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Helpers/Interfaces/ICalculationHelper.cs
@@ -0,0 +1,7 @@
+﻿namespace AvaliaAe.Helpers.Interfaces
+{
+    public interface ICalculationHelper
+    {
+        public float CalculateAverage(List<float> notes);
+    }
+}
